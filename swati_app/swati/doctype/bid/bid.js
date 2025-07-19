6e05d5d2991fc0364491791b62d909d36efe6758
gskishan <gskishan>
2025-07-19 08:34:58 +0000

    Linking the Bid document to Lead and Opportunity

--- swati_app/swati/doctype/bid/bid.js
+++ swati_app/swati/doctype/bid/bid.js
@@ -45,6 +45,43 @@ frappe.ui.form.on('Bid', {
                 frappe.set_route("Form", "Quotation", quotation.name);
             });
         }, __("Create"));
+        frm.add_custom_button('Sales Order', () => {
+            frappe.model.with_doctype('Sales Order', function() {
+                let sales_order = frappe.model.get_new_doc('Sales Order');
+        
+                // Copy fields from Bid
+                sales_order.contact_person = frm.doc.contact_person;
+                sales_order.customer_address = frm.doc.custom_site_address;
+        
+                // Copy items if available
+                if (frm.doc.items && frm.doc.items.length) {
+                    sales_order.items = [];
+                    frm.doc.items.forEach(item => {
+                        let new_item = frappe.model.add_child(sales_order, "Sales Order Item", "items");
+                        Object.assign(new_item, {
+                            item_code: item.item_code,
+                            item_name: item.item_name,
+                            qty: item.qty,
+                            rate: item.rate,
+                            uom: item.uom,
+                            brand: item.brand,
+                            item_group: item.item_group,
+                            description: item.description,
+                            image: item.image,
+                            image_view: item.image_view,
+                            base_rate: item.base_rate,
+                            base_amount: item.base_amount,
+                            amount: item.amount,
+                            custom_against_bid: frm.doc.name
+                        });
+                    });
+                }
+        
+                // Open new form with pre-filled data
+                frappe.set_route("Form", "Sales Order", sales_order.name);
+            });
+        }, __("Create"));
+        
     }
     }
 });
--- swati_app/swati/doctype/bid/bid.py
+++ swati_app/swati/doctype/bid/bid.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2025, CRUXEDGE and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Bid(Document):
-	pass
+    def before_save(self):
+        if not self.opportunity:
+            return
+        frappe.db.set_value("Opportunity",self.opportunity,"custom_bid",self.name)
