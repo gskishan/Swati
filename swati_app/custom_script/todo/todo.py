83d2986521cc5789570d039509febbe0218f36d9
G S Kishan <95840962+gskishan@users.noreply.github.com>
2025-12-19 12:02:03 +0530

    Update todo.py

--- swati_app/custom_script/todo/todo.py
+++ swati_app/custom_script/todo/todo.py
@@ -5,4 +5,5 @@ from frappe import _
 
 def validate(self, method):
     if self.date and self.date < getdate(today()):
-        frappe.throw(_("Backdated dates are not allowed in Todo. The date must be today or later."))
+        pass
+        #frappe.throw(_("Backdated dates are not allowed in Todo. The date must be today or later."))
